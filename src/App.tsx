972ccc2d1b899d0d8ccbe057fc7e8f9759188421
Rodrigo Fontenele Carvalho <1543402+drigofonte@users.noreply.github.com>
2021-09-30 03:41:29 +0200

    Moved the loading logic to the root to ensure data is loaded even if the user navigates directly to a specific profile page first

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,20 @@ import Container from '@mui/material/Container';
 import './App.css';
 import PeoplePage from './pages/People';
 import PersonProfilePage from './pages/PersonProfile';
+import { useStore } from './store/store';
+import Person from './models/person';
+import { useEffect } from 'react';
 
 function App() {
+  const [ state, dispatch ] = useStore(false);
+  const { people }: { people?: Person[] } = state;
+
+  useEffect(() => {
+    if (people?.length === 0) {
+      dispatch('LOAD_PEOPLE', null);
+    }
+  }, [dispatch, people]);
+
   return (
     <Container 
       maxWidth="md"
--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from 'react';
-import { useStore } from '../store/store';
-
 import Items from '../components/list/Items';
-import Person from '../models/person';
 
 const People = () => {
-  const [ state, dispatch ] = useStore(false);
-  const { people }: { people?: Person[] } = state;
-
-  useEffect(() => {
-    if (people?.length === 0) {
-      dispatch('LOAD_PEOPLE', null);
-    }
-  }, [dispatch, people]);
-
   return (
     <Items />
   );

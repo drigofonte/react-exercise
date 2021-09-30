831f3814e31f611aed14cf1c8d11c38c5699a579
Rodrigo Fontenele Carvalho <1543402+drigofonte@users.noreply.github.com>
2021-09-30 11:03:20 +0200

    Moved the data url to the .env file

--- src/models/person-loader.ts
+++ src/models/person-loader.ts
@@ -1,10 +1,15 @@
 import Person from "./person";
 
 class PersonLoader {
+  private static readonly DATA_URL: string | undefined = process.env.REACT_APP_DATA_URL;
+
   public static async getPeople(): Promise<Person[]> {
-    const res = await fetch('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json');
-    const { Brastlewark: data } = await res.json();
-    const people = data.map((d: {}) => Object.assign(new Person(), d));
+    let people = [];
+    if (this.DATA_URL) {
+      const res = await fetch(this.DATA_URL);
+      const { Brastlewark: data } = await res.json();
+      people = data.map((d: {}) => Object.assign(new Person(), d));
+    }
     return people;
   }
 }
